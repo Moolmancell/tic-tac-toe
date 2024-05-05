46e159ebb646aaea22da6eeef395be963fb5e4e8
Moolmancell <richmondclaudebodot@gmail.com>
2024-05-05 22:05:15 +0800

    Base functionality of the DOM with the console JS.

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-function Player(mark, order) {
+function Player(mark, order, name) {
     let score = 0;
     let position = order;
     const symbol = mark;
+    const playerName = name;
 
     return {
         score,
         position,
-        symbol
+        symbol,
+        playerName
     }
 }
 
-function Gameboard() {
+function controller(playerturn) {
+
+}
+
+const Gameboard = (() => {
+    const gameboardDOM = document.querySelector(".gameboard");
+    const gameboardCellsDOM  = document.querySelectorAll(".gameboard div")
+    const playerXName = document.querySelector("#nameX")
+    const player0Name = document.querySelector("#name0")
+
+    const startButton = document.querySelector(".startButton")
+
     let gameboard = [
         0,0,0,
         0,0,0,
@@ -20,25 +33,26 @@ function Gameboard() {
 
     return {
         gameboard,
+        gameboardDOM,
+        gameboardCellsDOM,
+        playerXName,
+        player0Name,
+        startButton
     }
-}
+})();
 
-const game = (function() {
-    const gameboard = Gameboard();
+function game(player1, player2) {
     let turn = 1; // Move the turn variable outside turn function
 
-    const player1 = Player("X", 1);
-    const player2 = Player("0", 2);
-
-    const reset = () => {
-        gameboard.gameboard = [
+    const again = () => {
+        Gameboard.gameboard = [
             0,0,0,
             0,0,0,
             0,0,0
         ];
     };
 
-    const checkMatch = (mark, score) => {
+    const checkEndMatch = (mark, score) => {
         const point = [
             [0,1,2],
             [3,4,5],
@@ -51,43 +65,63 @@ const game = (function() {
         ];
 
         point.forEach(element => {
-            if (gameboard.gameboard[element[0]] === mark &&
-                gameboard.gameboard[element[1]] === mark &&
-                gameboard.gameboard[element[2]] === mark) {
+            if (Gameboard.gameboard[element[0]] === mark &&
+                Gameboard.gameboard[element[1]] === mark &&
+                Gameboard.gameboard[element[2]] === mark) {
                     console.log(mark + " wins");
                     score+=1;
-                    reset();
+                    again();
                 } else {
                     return;
                 }
         });
     };
 
-    const place = (position) => {
-        console.log("asdf");
+    const place = (e) => {
+        const position = +e.target.getAttribute("data-cellnum");
         if (turn === 1) {
-            if (gameboard.gameboard[position] !== 0) {
+            if (Gameboard.gameboard[position] !== 0) {
                 console.log("Position Taken");
             } else {
-                gameboard.gameboard[position] = player1.symbol;
+                Gameboard.gameboard[position] = player1.symbol;
                 turn = 2;
-                console.log(gameboard.gameboard);
-                checkMatch(player1.symbol, player1.score)
+                console.log(Gameboard.gameboard);
+                checkEndMatch(player1.symbol, player1.score)
             }
         } else if (turn === 2) {
-            if (gameboard.gameboard[position] !== 0) {
+            if (Gameboard.gameboard[position] !== 0) {
                 console.log("Position Taken");
             } else {
-                gameboard.gameboard[position] = player2.symbol;
+                Gameboard.gameboard[position] = player2.symbol;
                 turn = 1;
-                console.log(gameboard.gameboard);
-                checkMatch(player2.symbol, player1.score)
+                console.log(Gameboard.gameboard);
+                checkEndMatch(player2.symbol, player1.score)
             }
         }
     };
+    Gameboard.gameboardCellsDOM.forEach(cell => {
+        cell.addEventListener("click", place);
+      });;
+};
 
-    return {
-        place,
-        gameboard
-    };
-})();
+function start() {
+    const orderTemp = Math.floor(Math.random() * 2) + 1;
+    let order1;
+    let order2;
+
+    if (orderTemp === 1) {
+        order1 = 1;
+        order2 = 2;
+    } else {
+        order1 = 2;
+        order2 = 1;
+    }
+
+    let name1 = Gameboard.playerXName.value;
+    let name2 = Gameboard.player0Name.value;
+    const player1 = Player("X", order1, name1)
+    const player2 = Player("0", order2, name2)
+    game(player1, player2);
+}
+
+Gameboard.startButton.onclick = start;
